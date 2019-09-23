a10201f85d97cb610d4714d297b233698a01335c
KY <hey_ky@foxmail.com>
2019-09-23 20:18:30 +0800

    update

--- src/LC_19.java
+++ src/LC_19.java
@@ -9,7 +9,8 @@ public class LC_19 {
             val = x;
         }
     }
-    //  暴力
+
+    //  2次遍历
     public ListNode removeNthFromEnd(ListNode head, int n) {
         int len = 0;
         ListNode cur = head;
@@ -18,21 +19,60 @@ public class LC_19 {
             len++;
         }
         if (n == len) {
-            head=head.next;
+            head = head.next;
             return head;
         } else if (n == 0) {
-            cur.next=null;
+            cur.next = null;
             return head;
         }
         cur = head;
         for (int i = 0; i < len - n - 1; i++) {
-            cur=cur.next;
+            cur = cur.next;
         }
-        if(n>1)
-            cur.next=cur.next.next;
+        if (n > 1)
+            cur.next = cur.next.next;
         else
-            cur.next=null;
+            cur.next = null;
         return head;
+    }
+
+    //  二次遍历, 用哨兵节点, 可减少边界条件的判断
+    public ListNode removeNthFromEnd_1(ListNode head, int n) {
+        int len = 0;
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            len++;
+        }
+        ListNode cur = pre;
+        for (int i = 0; i < len - n; i++) {
+            cur = cur.next;
+        }
+        if (n == 0) {
+            cur.next = null;
+        } else {
+            cur.next = cur.next.next;
+        }
+        return pre.next;
+    }
+
+    //  一次遍历, 用双指针
+    public ListNode removeNthFromEnd_2(ListNode head, int n) {
+        ListNode pre = new ListNode(0);
+        ListNode first = pre;
+        ListNode second = pre;
+        pre.next = head;
+        for (int i = 0; i < n + 1; i++) {
+            second = second.next;
+        }
+        for (; second != null; second = second.next) {
+            first = first.next;
+        }
+        if (n == 0)
+            first.next = null;
+        else
+            first.next = first.next.next;
+        return pre.next;
     }
 
 }

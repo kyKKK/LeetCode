95a34d4fe4c8b155c546b468851cc422b97dea12
KY <hey_ky@foxmail.com>
2020-08-07 21:37:21 +0800

    commit

--- src/LC_3/Solution.java
+++ src/LC_3/Solution.java
@@ -5,33 +5,17 @@ import java.util.Set;
 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
+        int left = 0, right = 0, max = 0;
         Set<Character> set = new HashSet<>();
-<<<<<<< HEAD
-        int right = -1, len = s.length();
-        int ret = 0;
-        for (int i = 0; i < len; i++) {
-            if (i > 0)
-                set.remove(s.charAt(i - 1));
-            while (right + 1 < len && !set.contains(s.charAt(right + 1))) {
-                set.add(s.charAt(++right));
-                ret = Math.max(ret, set.size());
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            while (set.contains(ch)) {
+                set.remove(s.charAt(left++));
             }
-=======
-        int right = -1, len = s.length(), ret = 0;
-        for (int i = 0; i < len; i++) {
-            if (i > 0)
-                set.remove(s.charAt(i - 1));
-            while (right + 1 < len) {
-                if (!set.contains(s.charAt(right + 1))) {
-                    set.add(s.charAt(++right));
-                    ret = Math.max(ret, set.size());
-                } else break;
-            }
-            if(len-1-i<=ret)
-                break;
-
->>>>>>> a826233b9970491afcae116efe05f5abdaf0bf93
+            set.add(ch);
+            right++;
+            max = Math.max(max, set.size());
         }
-        return ret;
+        return max;
     }
 }

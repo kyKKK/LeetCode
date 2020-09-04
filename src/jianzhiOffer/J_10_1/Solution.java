ede25ce11d84a832349a22bc082262050dcf1fdf
ky <hey_ky@foxmail.com>
2020-09-04 17:16:14 +0800

    update 2020/9/4

--- src/jianzhiOffer/J_10_1/Solution.java
+++ src/jianzhiOffer/J_10_1/Solution.java
@@ -2,17 +2,20 @@ package jianzhiOffer.J_10_1;
 
 public class Solution {
     public int fib(int n) {
-        if(n==0)
-            return 0;
-        int[] arr = new int[n + 1];
-        arr[1] = 1;
-        for (int i = 2; i < arr.length; i++) {
-            arr[i] = (arr[i - 2] + arr[i - 1])%(int)(1e9+7);
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return 1;
+        int a = 1, b = 1, sum = 0;
+        for (int i = 2; i <= n; i++) {
+            sum = (a + b) % (int) (1e9 + 7);
+            a = b;
+            b = sum;
         }
-        return arr[n];
+        return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().fib(81));
+        System.out.println(new Solution().fib(1));
     }
 }
---
+++ src/jianzhiOffer/J_11/Solution.java
@@ -0,0 +1,11 @@
+package jianzhiOffer.J_11;
+
+public class Solution {
+    public int minArray(int[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < numbers[i - 1])
+                return numbers[i];
+        }
+        return numbers[0];
+    }
+}
